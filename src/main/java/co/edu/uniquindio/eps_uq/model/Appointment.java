2f991fae0e99a0a95ed5cb899539873daa49175c
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2024-04-11 16:24:32 -0500

    Added some atributes to classes

---
+++ src/main/java/co/edu/uniquindio/eps_uq/model/Appointment.java
@@ -0,0 +1,24 @@
+package co.edu.uniquindio.eps_uq.model;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded =true)
+@AllArgsConstructor
+public class Appointment {
+	@EqualsAndHashCode.Include
+	private String id;
+	
+	
+	private LocalDate date;
+	private User user;
+	private PriorityLevel priorityLevel;
+	private Doctor doctor;
+	private String detail;
+	
+	
+}
