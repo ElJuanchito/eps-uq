d4498c3acf8c16d8fb9a7dd611754bfa8e2b6207
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2024-04-11 16:41:40 -0500

    Update Request.java

--- src/main/java/co/edu/uniquindio/eps_uq/model/Request.java
+++ src/main/java/co/edu/uniquindio/eps_uq/model/Request.java
@@ -1,5 +1,29 @@
 package co.edu.uniquindio.eps_uq.model;
 
-public class Request {
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded =true)
+@AllArgsConstructor
+public class Request implements Comparable<Request>{
+	
+	@EqualsAndHashCode.Include
+	private String id;
+	
+	
+	private LocalDate date;
+	private User user;
+	private PriorityLevel priorityLevel;
+	private Doctor doctor;
+	private String detail;
+	@Override
+	public int compareTo(Request o) {
+		return this.priorityLevel.getValue()-o.priorityLevel.getValue();
+	}
+	
 
 }
